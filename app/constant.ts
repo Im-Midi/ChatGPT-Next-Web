c5ce7af5c8c058047e058e99c8f3c6e75f3f0dc2
Im-Midi <86220188+Im-Midi@users.noreply.github.com>
2024-12-11 20:07:09 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -566,5 +566,5 @@ export const PLUGINS = [
   { name: "Search Chat", path: Path.SearchChat },
 ];
 
-export const SAAS_CHAT_URL = "https://nextchat.dev/chat";
+export const SAAS_CHAT_URL = "https://www.5yang.cc";
 export const SAAS_CHAT_UTM_URL = "https://www.5yang.cc";
